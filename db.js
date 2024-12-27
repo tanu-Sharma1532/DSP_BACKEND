328b404d3895c2a698bc3c615cd08677e49fd94e
atul <atul@atuls-MacBook-Air.local>
2024-12-27 15:46:15 +0530

    db

--- db.js
+++ db.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 
 
-
-
-    const mongoose = require('mongoose');
-
-// Connection URL with the database name specified
 const url = 'mongodb+srv://dsp91:tvaml0XcMkTOIwqL@dsp.cjqbl.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Dsp';
 
-// Connect to MongoDB
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
