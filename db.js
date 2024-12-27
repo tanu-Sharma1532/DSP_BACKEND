2b6b1df8763a60502646dd725a5d9aab2fbd1f5e
atul <atul@atuls-MacBook-Air.local>
2024-12-27 15:42:44 +0530

    added code

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://dsp91:tvaml0XcMkTOIwqL@dsp.cjqbl.mongodb.net/?retryWrites=true&w=majority&appName=Dsp';
+
+
+
+    const mongoose = require('mongoose');
+
+// Connection URL with the database name specified
+const url = 'mongodb+srv://dsp91:tvaml0XcMkTOIwqL@dsp.cjqbl.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Dsp';
 
 // Connect to MongoDB
-mongoose.connect(url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
+
