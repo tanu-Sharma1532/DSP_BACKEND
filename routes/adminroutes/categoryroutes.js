1d30d5074737bad381252a7b5ab68f1aa8f0a97a
Tanu Sharma <tanush1532@gmail.com>
2025-01-06 17:48:36 +0530

    updated

--- routes/adminroutes/categoryroutes.js
+++ routes/adminroutes/categoryroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const categoryController = require('../../controllers/adminControllers/categoryController');
+const categoryController = require('../../controllers/adminControllers/categorycontroller');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
